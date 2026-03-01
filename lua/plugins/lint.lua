66dcef5d5339a3f3a6acb5d2d4b4f94e85e0fd1c
Mark Tranter <tranter.m@sky.com>
2026-03-01 16:08:50 +0000

    first

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,26 @@
+return {
+
+  { -- Linting
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require 'lint'
+      lint.linters_by_ft = {
+        html = { 'htmlhint' },
+        bash = { 'shfmt' },
+        css = { 'stylelint' },
+        python = { 'ruff' }, -- Python linting
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = lint_augroup,
+        callback = function()
+          if vim.bo.modifiable then
+            lint.try_lint()
+          end
+        end,
+      })
+    end,
+  },
+}
---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,33 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  lazy = false,
+  keys = {
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
+  },
+  config = function()
+    require('neo-tree').setup {
+      close_if_last_window = true,
+      window = {
+        width = 24,
+      },
+    }
+  end,
+  opts = {
+    filesystem = {
+      window = {
+        mappings = {
+          ['\\'] = 'close_window',
+        },
+      },
+    },
+  },
+}
